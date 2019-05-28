74666cca6544223cfbfc5568365c56acbdc689ab
Weiping Xiang <weiping.xiang@shopee.com>
2019-05-28 17:51:09 +0800

    feat: build all project in mdx

---
+++ build/mdx-all.js
@@ -0,0 +1,19 @@
+const globby = require('globby');
+const shell = require('shelljs');
+
+
+const buildmdx = async () => {
+    const paths = await globby('mdx/*.mdx')
+
+    paths.forEach((path) => {
+        const name = path.match(/mdx\/(\w*).mdx/)
+
+        if(name[1]) {
+            shell.exec(`mdx-deck build ${path} -d web/${name[1]}`)
+            shell.echo(`${path} has been done`)
+        }
+
+    })
+}
+
+buildmdx()
