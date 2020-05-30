e8e3964dfde69045d19f0566a130f84567ff0dd1
Weiping Xiang <weiping.xiang@shopee.com>
2020-05-30 13:45:53 +0800

    update md

--- build/reveal-all.js
+++ build/reveal-all.js
@@ -1,51 +1,21 @@
-// var process = require('child_process');
 
-// const globby = require('globby');
-// const shell = require('shelljs');
-
-// const buildReveal = async () => {
-//   const paths = await globby('md/*.md')
-
- 
-
-//   paths.forEach((path) => {
-    
-//       const name = path.match(/md\/(\w*).md/)
-
-//       if(name[1]) {
-//           shell.exec(`reveal-md ./md/${name[1]}.md --static web`)
-//           shell.exec(`mv ./web/index.html ./web/${name[1]}.html`)
-//           shell.echo(`${name[1]} has been done`)
-//       }
-//   })
-
-//   shell.exec(`mkdir -p ./web/copy-img`);
-//   shell.exec(`cp ./md/copy-img/* ./web/copy-img`);
-// }
-
-// buildReveal()
-
-var process = require('child_process');
+var process = require("child_process");
+var globby = require("globby");
 
 
-var pageList = [
-  'vue-cli',
-  'vue-specification',
-  'vue-startup',
-  'vue-test',
-  'weapp',
-  'pwa',
-  'microfrontend',
-  'index',
-  'svelte',
-  'lerna'
-]
+const buildReveal = async () => {
+  const paths = await globby("md/*.md");
 
-pageList.forEach((page)=> {
-    process.execSync(`reveal-md ./md/${page}.md --static web`);
-    process.execSync(`mv ./web/index.html ./web/${page}.html`);
-})
+  paths.forEach((path) => {
+    console.log(path);
+    const name = path.match(/md\/((\w|-)*)\.md/);
 
-// process.execSync(`mkdir -p ./web/copy-img`);
-// process.execSync(`cp ./md/copy-img/* ./web/copy-img`);
+    if (name[1]) {
+      var page = name[1];
+      process.execSync(`npx reveal-md ./md/${page}.md --static web`);
+      console.log(`${page}.html is successfully converted!!!`);
+    }
+  });
+}
 
+buildReveal()
